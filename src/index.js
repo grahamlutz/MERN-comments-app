3748a4af6de78ded5f8063c0afe4a8d05a8e3474
Graham Lutz <graham@brainbytescreative.com>
2017-06-16 15:22:00 -0400

    creates file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+//index.js
+import React from ‘react’;
+import ReactDOM from ‘react-dom’;
+import CommentBox from ‘./CommentBox’;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+ <CommentBox />,
+ document.getElementById(‘root’)
+);
